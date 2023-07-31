c9b70ec8d22322b9f3aab35d4b95b5688538fbb2
TedXpro <johannes.woldeyes@gmail.com>
2023-07-31 15:58:39 +0300

    add controller and Test classes

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -19,13 +19,27 @@ public class CashCardJsonTest {
         CashCard cashCard = new CashCard(99L, 123.45);
 
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
-        
+
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
-        
+
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
 
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
-        
+
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+    
+    @Test
+    public void cashCardDeserialization() throws IOException {
+        String expected = """
+                {
+                    "id":99,
+                    "amount":123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
